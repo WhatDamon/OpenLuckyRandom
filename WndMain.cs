288ee3ef3007cbde240feb4b948f8c69702807ee
Damon  Lu <59256766+WhatDamon@users.noreply.github.com>
2024-12-01 00:12:54 +0800

    fix: try to fix some problem, step 1

--- WndMain.cs
+++ WndMain.cs
@@ -18,6 +18,7 @@ namespace OpenLuckyRandom
         // 初始化
         private VideoCapture capture;
         private Mat frame = new Mat();
+        private Mat grayFrame = new Mat();
         private CascadeClassifier faceCascade;
         private PerformanceCounter cpuCounter;
         private int frameThickness = 6;
@@ -26,6 +27,7 @@ namespace OpenLuckyRandom
             "haarcascade_frontalface_alt.xml",
             "haarcascade_frontalface_alt2.xml"
         };
+        private Random rand = new Random();
 
         public WndMain()
         {
@@ -104,7 +106,7 @@ namespace OpenLuckyRandom
         }
 
         // 加载人脸级联分类器
-        private void LoadFaceCascade(string fileName)
+        private async void LoadFaceCascade(string fileName)
         {
             // 释放之前的 CascadeClassifier 实例
             if (faceCascade != null)
@@ -117,15 +119,18 @@ namespace OpenLuckyRandom
             string xmlPath = Path.Combine(Application.StartupPath, "cascades/", fileName);
             try
             {
-                faceCascade = new CascadeClassifier(xmlPath);
-                if (faceCascade.Empty())
+                await Task.Run(() =>
                 {
-                    currentStatusLabel.Text = $"人脸级联分类器 {fileName} 加载失败";
-                }
-                else
-                {
-                    currentStatusLabel.Text = $"成功加载人脸级联分类器: {fileName}";
-                }
+                    faceCascade = new CascadeClassifier(xmlPath);
+                    if (faceCascade.Empty())
+                    {
+                        currentStatusLabel.Text = $"人脸级联分类器 {fileName} 加载失败";
+                    }
+                    else
+                    {
+                        currentStatusLabel.Text = $"成功加载人脸级联分类器: {fileName}";
+                    }
+                });
             }
             catch (Exception ex)
             {
@@ -140,9 +145,9 @@ namespace OpenLuckyRandom
         }
 
         // 点击刷新摄像头按钮
-        private void refreshCameraBtn_Click(object sender, EventArgs e)
+        private async void refreshCameraBtn_Click(object sender, EventArgs e)
         {
-            CameraDevicesLoad();
+            await Task.Run(() => CameraDevicesLoad());
         }
 
         private void InitializeCapture(int cameraIndex)
@@ -188,26 +193,26 @@ namespace OpenLuckyRandom
             frameThickness = Convert.ToInt32(frameThicknessNum.Value);
         }
 
-        private void captureTimer_Tick(object sender, EventArgs e)
+        private async void captureTimer_Tick(object sender, EventArgs e)
         {
-            // 检查 faceCascade 是否已正确加载
-            if (faceCascade == null || faceCascade.Empty())
+            await Task.Run(() =>
             {
-                currentStatusLabel.Text = "人脸级联分类器未加载";
-                return;
-            }
+                // 检查 faceCascade 是否已正确加载
+                if (faceCascade == null || faceCascade.Empty())
+                {
+                    this.Invoke((Action)(() => currentStatusLabel.Text = "人脸级联分类器未加载"));
+                    return;
+                }
 
-            // 从摄像头读取一帧图像
-            capture.Read(frame);
-            if (frame.Empty())
-            {
-                currentStatusLabel.Text = "无法读取摄像头数据";
-                captureTimer.Stop(); // 停止计时器，防止爆内存或者CPU
-                return;
-            }
+                // 从摄像头读取一帧图像
+                capture.Read(frame);
+                if (frame.Empty())
+                {
+                    this.Invoke((Action)(() => currentStatusLabel.Text = "无法读取摄像头数据"));
+                    captureTimer.Stop(); // 停止计时器，防止爆内存或者CPU
+                    return;
+                }
 
-            using (Mat grayFrame = new Mat()) // 转换为灰度图像
-            {
                 Cv2.CvtColor(frame, grayFrame, ColorConversionCodes.BGR2GRAY);
 
                 // 检测人脸
@@ -222,19 +227,20 @@ namespace OpenLuckyRandom
                 // 更新状态标签
                 if (faces.Length > 0)
                 {
-                    faceRecogStatusLabel.Text = $"检测到 {faces.Length} 个人脸";
-                    randomBtn.Enabled = true;
+                    this.Invoke((Action)(() => faceRecogStatusLabel.Text = $"检测到 {faces.Length} 个人脸"));
+                    this.Invoke((Action)(() => randomBtn.Enabled = true));
                 }
                 else
                 {
-                    faceRecogStatusLabel.Text = "未识别到人脸";
-                    randomBtn.Enabled = false;
+                    this.Invoke((Action)(() => faceRecogStatusLabel.Text = "未识别到人脸"));
+                    this.Invoke((Action)(() => randomBtn.Enabled = false));
                 }
-            }
 
-            // 将Mat转换为Bitmap，并显示在PictureBox中
-            cameraCurrent.Image = frame.ToBitmap();
+                // 将Mat转换为Bitmap，并显示在PictureBox中
+                this.Invoke((Action)(() => cameraCurrent.Image = frame.ToBitmap()));
+            });
         }
+
 
         // 窗口关闭
         private void WndMain_FormClosing(object sender, FormClosingEventArgs e)
@@ -271,55 +277,56 @@ namespace OpenLuckyRandom
             backBtn.Enabled = true;
 
             // 检测人脸
-            Mat grayFrame = new Mat();
-            Cv2.CvtColor(frame, grayFrame, ColorConversionCodes.BGR2GRAY);
-            Rect[] faces = faceCascade.DetectMultiScale(grayFrame, 1.1, 10);
-
-            if (faces.Length > 0)
+            using (Mat grayFrame = new Mat())
             {
-                // 使用随机数算法确定选择哪个人
-                Random rand = new Random();
-                int selectedFaceIndex = rand.Next(faces.Length);
-                Rect selectedFace = faces[selectedFaceIndex];
+                Cv2.CvtColor(frame, grayFrame, ColorConversionCodes.BGR2GRAY);
+                Rect[] faces = faceCascade.DetectMultiScale(grayFrame, 1.1, 10);
 
-                // 强调幸运儿
-                frame.ConvertTo(frame, MatType.CV_8UC3);
-                Cv2.Rectangle(frame, selectedFace, new Scalar(150, 255, 150), frameThickness + 4);
-                Cv2.Rectangle(frame, selectedFace, Scalar.Green, frameThickness - 1);
+                if (faces.Length > 0)
+                {
+                    // 使用随机数算法确定选择哪个人
+                    int selectedFaceIndex = rand.Next(faces.Length);
+                    Rect selectedFace = faces[selectedFaceIndex];
 
-                // 绘制蓝色箭头
-                OpenCvSharp.Point endPoint = new OpenCvSharp.Point(selectedFace.X + selectedFace.Width / 2, selectedFace.Y - 20);
-                OpenCvSharp.Point startPoint = new OpenCvSharp.Point(endPoint.X, endPoint.Y - Math.Max(selectedFace.Height / 2, 50));
+                    // 强调幸运儿
+                    frame.ConvertTo(frame, MatType.CV_8UC3);
+                    Cv2.Rectangle(frame, selectedFace, new Scalar(150, 255, 150), frameThickness + 4);
+                    Cv2.Rectangle(frame, selectedFace, Scalar.Green, frameThickness - 1);
 
-                double arrowLength = 20;
-                double angle = Math.PI / 6;
-                OpenCvSharp.Point leftTip = new OpenCvSharp.Point(
-                    (int)(endPoint.X - arrowLength * Math.Cos(angle)),
-                    (int)(endPoint.Y - arrowLength * Math.Sin(angle))
-                );
-                OpenCvSharp.Point rightTip = new OpenCvSharp.Point(
-                    (int)(endPoint.X + arrowLength * Math.Cos(angle)),
-                    (int)(endPoint.Y - arrowLength * Math.Sin(angle))
-                );
+                    // 绘制蓝色箭头
+                    OpenCvSharp.Point endPoint = new OpenCvSharp.Point(selectedFace.X + selectedFace.Width / 2, selectedFace.Y - 20);
+                    OpenCvSharp.Point startPoint = new OpenCvSharp.Point(endPoint.X, endPoint.Y - Math.Max(selectedFace.Height / 2, 50));
 
-                Cv2.Line(frame, startPoint, endPoint, new Scalar(255, 0, 119, 215), frameThickness);
-                Cv2.Line(frame, endPoint, leftTip, new Scalar(255, 0, 119, 215), frameThickness);
-                Cv2.Line(frame, endPoint, rightTip, new Scalar(255, 0, 119, 215), frameThickness);
+                    double arrowLength = 20;
+                    double angle = Math.PI / 6;
+                    OpenCvSharp.Point leftTip = new OpenCvSharp.Point(
+                        (int)(endPoint.X - arrowLength * Math.Cos(angle)),
+                        (int)(endPoint.Y - arrowLength * Math.Sin(angle))
+                    );
+                    OpenCvSharp.Point rightTip = new OpenCvSharp.Point(
+                        (int)(endPoint.X + arrowLength * Math.Cos(angle)),
+                        (int)(endPoint.Y - arrowLength * Math.Sin(angle))
+                    );
 
-                // 显示结果
-                cameraCurrent.Image = frame.ToBitmap();
-                currentStatusLabel.Text = $"抽中索引值为 {selectedFaceIndex} 的脸，孩子你中了！";
+                    Cv2.Line(frame, startPoint, endPoint, new Scalar(255, 0, 119, 215), frameThickness);
+                    Cv2.Line(frame, endPoint, leftTip, new Scalar(255, 0, 119, 215), frameThickness);
+                    Cv2.Line(frame, endPoint, rightTip, new Scalar(255, 0, 119, 215), frameThickness);
+
+                    // 显示结果
+                    cameraCurrent.Image = frame.ToBitmap();
+                    currentStatusLabel.Text = $"抽中索引值为 {selectedFaceIndex} 的脸，孩子你中了！";
+                }
+                else
+                {
+                    // 组件状态恢复
+                    configGroupBox.Enabled = true;
+                    randomBtn.Enabled = true;
+                    backBtn.Enabled = false;
+                    currentStatusLabel.Text = "就绪";
+                }
 
                 // 释放资源
                 GC.Collect();
-            }
-            else
-            {
-                // 组件状态恢复
-                configGroupBox.Enabled = true;
-                randomBtn.Enabled = true;
-                backBtn.Enabled = false;
-                currentStatusLabel.Text = "就绪";
             }
         }
 
